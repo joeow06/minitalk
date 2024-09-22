85ad46797bab4f02799adef3a018d3fb967c6c8b
Joe Ow <joeow@Joes-MacBook-Air.local>
2024-09-22 22:40:48 +0800

    added

---
+++ client.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jow <jow@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 18:20:13 by jow               #+#    #+#             */
+/*   Updated: 2024/09/22 22:38:49 by jow              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "inc/minitalk.h"
+
+int	g_confirm_flag = 0;
+
+void ft_resp_handler(int signum)
+{
+	g_confirm_flag = 1;
+	(void)signum;
+}
+
+void	ft_send_signals(int server_pid, char *message)
+{
+	int				letter;
+	int				i;
+
+	letter = 0;
+	while (message[letter])
+	{
+		i = -1;
+		while (++i < 8)
+		{
+			if (((unsigned char)(message[letter] >> (7 - i)) & 1) == 0)
+				kill(server_pid, SIGUSR1);
+			else if (((unsigned char)(message[letter] >> (7 - i)) & 1) == 1)
+				kill(server_pid, SIGUSR2);
+			while (!g_confirm_flag)
+				;
+			g_confirm_flag = 0;
+			usleep(100);
+		}
+		letter++;
+	} 
+}
+
+int	main(int argc, char **argv)
+{
+	char	*message;
+	int		server_pid;
+
+	if (argc == 3)
+	{
+		server_pid = ft_atoi(argv[1]);
+		if (server_pid <= 0)
+		{
+			ft_printf("Error PID\n");
+			return (0);
+		}
+		message = argv[2];
+		if (message[0] == 0)
+		{
+			ft_printf("Error Message\n");
+			return (0);
+		}
+		signal(SIGUSR2, ft_resp_handler);
+		ft_send_signals(server_pid, message);
+	}
+	else
+		ft_printf("Error Arguments\n");
+	return (0);
+}
---
+++ test_client.c
@@ -0,0 +1,66 @@
+#include "inc/minitalk.h"
+
+int g_confirm_flag = 0;
+
+void ft_resp_handler(int signum)
+{
+	g_confirm_flag = 1;
+	(void)signum;
+}
+
+void ft_send_bit(int pid, int bit)
+{
+	int signal;
+
+	if (bit == 1)
+		signal = SIGUSR1;
+	else
+		signal = SIGUSR2;
+	if (kill(pid, signal) == -1)
+	{
+		ft_putstr_fd("Error", 2);
+		exit(EXIT_FAILURE);
+	}
+	while (!g_confirm_flag)
+		;
+	g_confirm_flag = 0;
+}
+
+void ft_send_char(int pid, unsigned char c)
+{
+	int i;
+
+	i = 7;
+	while (i >= 0)
+	{
+		ft_send_bit(pid, (c >> i) & 1);
+		i--;
+	}
+}
+
+void ft_send_string(int pid, const char *str)
+{
+	while (*str)
+		ft_send_char(pid, *str++);
+	ft_send_char(pid, '\0');
+}
+
+int main(int argc, char **argv)
+{
+	pid_t pid;
+
+	if (argc != 3)
+	{
+		ft_putstr_fd("Usage: ./client [PID] [string]\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	pid = ft_atoi(argv[1]);
+	if (pid <= 0)
+	{
+		ft_putstr_fd("Invalid PID\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	signal(SIGUSR2, ft_resp_handler);
+	ft_send_string(pid, argv[2]);
+	return (0);
+}
